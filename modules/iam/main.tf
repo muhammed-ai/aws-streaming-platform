c77ab632886cc72db808996bb0a115bc1768de19
Muhammed Ibrahim <134043418+muhammed-ai@users.noreply.github.com>
2026-04-19 15:40:52 -0700

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -4,19 +4,9 @@ resource "aws_iam_role" "lambda_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow",
+      Effect    = "Allow",
       Principal = { Service = "lambda.amazonaws.com" },
-      Action = "sts:AssumeRole"
+      Action    = "sts:AssumeRole"
     }]
   })
-}
-
-resource "aws_lambda_function" "api" {
-  function_name = "netflix-api-${var.env}"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-
-  # placeholder zip (empty file for now)
-  filename = "${path.module}/lambda.zip"
 }
